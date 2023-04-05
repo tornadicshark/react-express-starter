67b754a9691684208915075bfc0b6d42ed6bfe0e
kwalke79 <kwalke79@uncc.edu>
2023-04-05 07:15:42 -0400

    Localhost is running.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
